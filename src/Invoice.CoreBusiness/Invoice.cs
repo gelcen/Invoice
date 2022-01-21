b1a15e52e5c77a32cfb945d8aa4e7545b4398168
Gelcen Savynday <gelcen.savynday@grapecity.com>
2022-01-21 19:47:44 +0700

    Added CoreBusiness project.

---
+++ src/Invoice.CoreBusiness/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Invoice.CoreBusiness
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime ModifiedAt { get; set; }
+        public ProcessingStatus ProcessingStatus { get; set; }
+        public float Amount { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
